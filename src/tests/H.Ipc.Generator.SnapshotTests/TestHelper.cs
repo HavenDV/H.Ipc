7febad8c9e10467ce583ad5abcf5cd5e366789d4
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2022-03-12 12:23:32 +1000

    test: Added common diagnostics compare to SnapshotTests.

--- src/tests/H.Ipc.Generator.SnapshotTests/TestHelper.cs
+++ src/tests/H.Ipc.Generator.SnapshotTests/TestHelper.cs
@@ -5,23 +5,47 @@ namespace H.Ipc.Generator.IntegrationTests;
 
 public static class TestHelper
 {
-    public static Task CheckSource(this VerifyBase verifier, string source)
+    public static async Task CheckSource(this VerifyBase verifier, string source)
     {
-        var compilation = CSharpCompilation.Create(
+        var dotNetFolder = Path.GetDirectoryName(typeof(object).Assembly.Location) ?? string.Empty;
+        var compilation = (Compilation)CSharpCompilation.Create(
             assemblyName: "Tests",
             syntaxTrees: new[]
             {
                 CSharpSyntaxTree.ParseText(source),
+                CSharpSyntaxTree.ParseText(@"
+namespace MyCode
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
+"),
             },
             references: new[]
             {
                 MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                MetadataReference.CreateFromFile(Path.Combine(dotNetFolder, "System.Runtime.dll")),
+                MetadataReference.CreateFromFile(typeof(H.Pipes.PipeClient<>).Assembly.Location),
             });
         var generator = new HIpcGenerator();
         var driver = (GeneratorDriver)CSharpGeneratorDriver.Create(generator);
         driver = driver.RunGenerators(compilation);
 
-        return verifier
+        driver = driver.RunGeneratorsAndUpdateCompilation(
+            compilation,
+            out compilation,
+            out _);
+        var diagnostics = compilation.GetDiagnostics();
+
+        await verifier
+            .Verify(diagnostics)
+            .UseDirectory("Snapshots")
+            .UseTextForParameters("Diagnostics");
+        await verifier
             .Verify(driver)
             .UseDirectory("Snapshots");
     }
