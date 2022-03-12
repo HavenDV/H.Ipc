41dc07b16557c204840e0dbf1aaa44e756a12302
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2022-03-12 12:47:23 +1000

    fix: Removed net5/net6 targets from H.Ipc.Core.

--- src/tests/H.Ipc.Generator.SnapshotTests/TestHelper.cs
+++ src/tests/H.Ipc.Generator.SnapshotTests/TestHelper.cs
@@ -29,6 +29,7 @@ namespace MyCode
             {
                 MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
                 MetadataReference.CreateFromFile(Path.Combine(dotNetFolder, "System.Runtime.dll")),
+                MetadataReference.CreateFromFile(Path.Combine(dotNetFolder, "netstandard.dll")),
                 MetadataReference.CreateFromFile(typeof(H.Pipes.PipeClient<>).Assembly.Location),
                 MetadataReference.CreateFromFile(typeof(H.IpcGenerators.IpcClientAttribute).Assembly.Location),
             });
